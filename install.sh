95b205410e1227a8a18cfb8ecda9d87b7302c78d
kuruteiru <martin.krutil@email.cz>
2024-11-15 15:53:55 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DOTFILES_DIR="~/.dotfiles"
+DOTFILES_DIR=~/.dotfiles
 
-ln -sf "$DOTFILES_DIR/dnf/dnf.conf" /etc/dnf/dnf.conf
-ln -sf "$DOTFILES_DIR/kitty" ~/.config/kitty
-ln -sf "$DOTFILES_DIR/nvim" ~/.config/nvim
-ln -sf "$DOTFILES_DIR/sway" ~/.config/sway
-ln -sf "$DOTFILES_DIR/waybar" ~/.config/waybar
+ln -sf $DOTFILES_DIR/dnf/dnf.conf /etc/dnf/dnf.conf
+ln -sf $DOTFILES_DIR/kitty ~/.config/kitty
+ln -sf $DOTFILES_DIR/nvim ~/.config/nvim
+ln -sf $DOTFILES_DIR/sway ~/.config/sway
+ln -sf $DOTFILES_DIR/waybar ~/.config/waybar
 
 if [[ -d "$FIREFOX_PROFILES_DIR" ]]; then
   for PROFILE in "$FIREFOX_PROFILES_DIR"/*-default; do
