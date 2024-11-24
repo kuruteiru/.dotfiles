39c6f055a7ce5ab5dd0f9c5273c8887a7cdc6873
kuruteiru <martin.krutil@email.cz>
2024-11-24 18:24:30 +0100

    formatting

--- sway/clamshell.sh
+++ sway/clamshell.sh
@@ -6,7 +6,7 @@ LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 read -r LS < "$LID_STATE_FILE"
 
 case "$LS" in
-*open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
-*)       echo "Could not get lid state" >&2 ; exit 1 ;;
+    *open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
+    *closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
+    *)       echo "Could not get lid state" >&2 ; exit 1 ;;
 esac
