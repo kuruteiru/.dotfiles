e63dcda07d379361ff3cbf495106a9e20e8b0a18
kuruteiru <martin.krutil@email.cz>
2024-12-29 18:12:57 +0100

    fixed linking

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 DOTFILES_DIR=~/.dotfiles
 
-ln -sf $DOTFILES_DIR/bash/bashrc ~/.bashrc
-ln -sf $DOTFILES_DIR/tmux/ ~/.config/tmux.conf
-ln -sf $DOTFILES_DIR/kitty ~/.config/kitty
-ln -sf $DOTFILES_DIR/nvim ~/.config/nvim
-ln -sf $DOTFILES_DIR/sway ~/.config/sway
-ln -sf $DOTFILES_DIR/waybar ~/.config/waybar
-ln -sf $DOTFILES_DIR/zathura ~/.config/zathura
+ln -sfT $DOTFILES_DIR/bash/bashrc ~/.bashrc
+ln -sfT $DOTFILES_DIR/tmux/ ~/.config/tmux.conf
+ln -sfT $DOTFILES_DIR/kitty ~/.config/kitty
+ln -sfT $DOTFILES_DIR/ghostty ~/.config/ghostty
+ln -sfT $DOTFILES_DIR/nvim ~/.config/nvim
+ln -sfT $DOTFILES_DIR/sway ~/.config/sway
+ln -sfT $DOTFILES_DIR/waybar ~/.config/waybar
+ln -sfT $DOTFILES_DIR/zathura ~/.config/zathura
 
 # needs to be used with root privilages, but when calling this script with sudo, the DOTFILES_DIR is /root/.dotfiles
 # sudo ln -sf $DOTFILES_DIR/dnf/dnf.conf /etc/dnf/dnf.conf
