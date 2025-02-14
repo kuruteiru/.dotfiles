0396865d7a5493cb2c1515e400718ae6c8e7e615
kuruteiru <martin.krutil@email.cz>
2025-02-14 01:02:26 +0100

    lsps

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,20 +4,27 @@ local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local servers = {
-    "pyright",
-    "clangd",
-    "gopls",
-    "eslint",
-    "html",
-    "jsonls",
-    "cssls",
-	"marksman"
+	"pyright",
+	"clangd",
+	"gopls",
+	"eslint",
+	"html",
+	"jsonls",
+	"cssls",
+	"marksman",
+	"sqls"
+
+	-- "css-variables-language-server",
+	-- "docker-compose-language-service",
+	-- "dockerfile-language-server",
+	-- "lua-language-server",
+	-- "cmake-language-server",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup {
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	}
 end
