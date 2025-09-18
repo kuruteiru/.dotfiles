f0fb192b872db051c91256e9ec54f59362f6523e
kuruteiru <martin.krutil@email.cz>
2025-09-18 16:04:46 +0200

    java

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,49 +3,32 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 local nvlsp     = require "nvchad.configs.lspconfig"
 
-local servers   = {
-	"pyright",
-	"clangd",
-	"gopls",
-	"eslint",
-	"html",
-	"jsonls",
-	"cssls",
-	"marksman",
-	"sqls",
-	"csharp_ls",
-	"ts_ls",
-	"denols",
-	"tailwindcss",
-	-- "ltex",
-	"lua_ls",
-	"vuels",
-	"bashls"
+local servers = {
+  "pyright",
+  "clangd",
+  "gopls",
+  "eslint",
+  "html",
+  "jsonls",
+  "cssls",
+  "marksman",
+  "sqls",
+  "csharp_ls",
+  "ts_ls",
+  "denols",
+  "tailwindcss",
+  -- "ltex",
+  "lua_ls",
+  "vuels",
+  "bashls",
+  "angularls", -- Angular LSP
+  "jdtls",     -- Java
 }
---
---
--- lspconfig.ts_ls.setup {
--- 	filetypes    = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "html" },
--- 	on_attach    = nvlsp.on_attach,
--- 	on_init      = nvlsp.on_init,
--- 	capabilities = nvlsp.capabilities,
--- }
---
--- lspconfig.html.setup {
--- 	filetypes    = { "html" },
--- 	init_options = {
--- 		embeddedLanguages = { css = true, javascript = true, },
--- 		provideFormatter = false,
--- 	},
--- 	on_attach    = nvlsp.on_attach,
--- 	on_init      = nvlsp.on_init,
--- 	capabilities = nvlsp.capabilities,
--- }
 
 for _, name in ipairs(servers) do
-	lspconfig[name].setup {
-		on_attach    = nvlsp.on_attach,
-		on_init      = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	}
+  lspconfig[name].setup {
+    on_attach    = nvlsp.on_attach,
+    on_init      = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
 end
