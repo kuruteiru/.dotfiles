242d1a939663b2169b524b607badf220453ada79
kuruteiru <martin.krutil@email.cz>
2026-01-18 00:45:31 +0100

    render-markdown plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,30 +1,39 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		opts = require "configs.conform",
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require "configs.lspconfig"
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim", "lua", "vimdoc", "html", "css", "c_sharp", "c", "cpp", "go", "javascript", "python", "rust",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim", "lua", "vimdoc", "html", "css", "c_sharp", "c", "cpp", "go", "javascript", "python", "rust",
+			},
+		},
+	},
 
-  {
-    "mfussenegger/nvim-jdtls",
-    ft = { "java" },
-    config = function()
-      require("configs.jdtls").setup()
-    end
-  }
+	{
+		"mfussenegger/nvim-jdtls",
+		ft = { "java" },
+		config = function()
+			require("configs.jdtls").setup()
+		end
+	},
+
+	{
+		'MeanderingProgrammer/render-markdown.nvim',
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+		---@module 'configs.render-markdown'
+		-- -@type render.md.UserConfig
+		ft = { 'markdown', 'quarto' },
+		opts = {},
+	}
 }
